b8a8f0af271d6a2141997725ea6384f62a426f43
Jose Reginaldo <jose.junior2@iesb.br>
2020-09-02 11:08:18 -0300

    aula de variaveis e constantes

---
+++ Aulas/02-VariaveisConstantes/declaracao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int numero = 100;
+char ch = 'A';
+unsigned char var = 10;
+const int SEXO_MASCULINO = 1;
+
+int main() {
+
+    printf("%d", numero);
+    printf("%c", ch);
+    return 0;
+
+}
