c2187ffa4c6667ee84ad4820329850ea64e27d71
Milena <milenliveira@gmail.com>
2020-10-03 16:45:26 -0300

    Operadores (atribuição)

---
+++ Aulas/03 - Operadores/atribuicao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int var = 10; // atribuição, o igual (=) no C, você atribui algo a variavel
+
+    printf ("%d\n", var);
+
+    var = 20; // atribuição
+
+    printf("%d\n", var);
+
+    return 0;
+
+}
