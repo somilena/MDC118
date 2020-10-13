65f56ddccd75f71f3754c09eeb87edd0a0af0e6e
Milena <milenliveira@gmail.com>
2020-10-12 23:10:07 -0300

    Desconto

--- Atividades/Atividade 02/02.c
+++ Atividades/Atividade 02/02.c
@@ -8,8 +8,43 @@ sobre um valor com base na escala: até de 100.00 aplica
 
 int main () {
 
+    float valor;
+    float desconto;
+    float preco;
+
+    printf("\n");
+    printf("Informe o valor para calcular o desconto: ");
+    scanf("%f", &valor);
+    printf("                ~~~~~~~~~~\n");
 
 
+    if (valor <= 100)
+    {
+        desconto = valor * 0.01;
+        preco = valor - desconto;
+        printf("O preço com o desconto fica R$ %.2f reais.\n", preco);
+        printf("                ~~~~~~~~~~\n");
+        printf("Desconto de 1%% aplicado!\n");
+    }
+    if (valor >= 100.01 && valor < 500)
+    {
+        desconto = valor * 0.05;
+        preco = valor - desconto;
+        printf("O preço com o desconto fica R$ %.2f reais.\n", preco);
+        printf("                ~~~~~~~~~~\n");
+        printf("Desconto de 5%% aplicado!\n");
+    }
+    if (valor > 500)
+    {
+        desconto = valor * 0.1;
+        preco = valor - desconto;
+        printf("O preço com o desconto fica R$ %.2f reais.\n", preco);
+        printf("                ~~~~~~~~~~\n");
+        printf ("Desconto de 10%% aplicado!");
+    }
+
+    printf("\n");
+    
 
     return 0;
 }
