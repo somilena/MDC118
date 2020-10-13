f16c3c6fddd164c8a7b111bafe0233d4882aa48f
Milena <milenliveira@gmail.com>
2020-10-13 19:07:05 -0300

    Fatorial

--- Atividades/Atividade 03/03.c
+++ Atividades/Atividade 03/03.c
@@ -1,13 +1,22 @@
 /* 3. Escrever um programa em C que calcule o fatorial
 de um número inteiro. */
 
-
 #include <stdio.h>
 
-int main () {
+int main()
+{
 
+    int fatorial;
+    int numero;
 
+    printf("Qual número você deseja calcular para fatorial? ");
+    scanf("%d", &numero);
 
+    for (fatorial = 1; numero > 1; numero--)
+    {
+        fatorial = fatorial * numero;
+    }
+    printf("O fatorial calculado: %d\n", fatorial);
 
     return 0;
 }
