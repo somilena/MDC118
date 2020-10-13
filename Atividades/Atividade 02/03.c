25c31e4f8906946780a51a22c430d65871f973e1
Milena <milenliveira@gmail.com>
2020-10-13 18:31:52 -0300

    Nota

--- Atividades/Atividade 02/03.c
+++ Atividades/Atividade 02/03.c
@@ -9,7 +9,31 @@ tabela: 1 = Ruim; 2 = Insuficiente; 3 = Suficiente,
 int main () {
 
 
+    int valor;
 
+    printf("Qual foi a sua nota? ");
+    scanf("%d", &valor);
+
+    switch (valor)
+    {
+    case 1: 
+        printf("%s\n", "Eita, você está com uma nota RUIM, estude mais!");
+        break;
+    case 2:
+        printf("%s\n", "Eita, você está com uma nota INSUFICIENTE, estude mais!");
+        break;
+    case 3:
+        printf("%s\n", "Parabéns, você está com uma nota SUFICIENTE para passar!");
+        break;
+    case 4:
+        printf("%s\n", "Parabéns, você está com uma BOA nota!");
+    case 5:
+        printf("%s\n", "Parabéns, você está com uma ÓTIMA nota!");
+        break;
+    default: 
+        printf("%s\n", "Ops! Erro! O sistema aceita somente notas no valor de 1 a 5!");
+        break; 
+    }
 
     return 0;
 }
