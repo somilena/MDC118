9744d769e35be38dce849b76bddedd011edced70
Milena <milenliveira@gmail.com>
2020-10-12 23:06:52 -0300

    Perímetro

--- Atividades/Atividade 01/01.c
+++ Atividades/Atividade 01/01.c
@@ -7,8 +7,18 @@ pi = 3.1416). */
 
 int main () {
 
+    int perimetro;
+    int raio;
+    int pi = 3.1416;
 
+    printf("---> Digite o valor do raio: ");
+    scanf("%d", &raio);
 
+    perimetro = 2 * pi * raio;
+
+    printf("         ~~~~~~~~~~~\n");
+
+    printf("O valor do perímetro é: %d\n", perimetro);
 
     return 0;
 }
