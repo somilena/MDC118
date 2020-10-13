b1ea7e2acaa15ef8a31b9be63503220fefaf8b5d
Milena <milenliveira@gmail.com>
2020-10-12 23:07:15 -0300

    Segundos

--- Atividades/Atividade 01/02.c
+++ Atividades/Atividade 01/02.c
@@ -6,8 +6,22 @@
 
 int main () {
 
+    int x;
+    int segundos;
+    int minutos;
+    int horas;
+
+    printf("Digite um número em segundos para ser convetido em segundos, minutos e horas: ");
+    scanf("%d", &x);
 
 
+    segundos = x;
+    minutos = x / 60;
+    horas = x / 3600;
+    
+    printf("          ~~~~~~~~~~\n");
+
+    printf("O valor é: %d segundos, %d minutos e %d horas.\n", segundos, minutos, horas);
 
     return 0;
 }
