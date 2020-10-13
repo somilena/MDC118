ddbdf671f8dc6c5fed5f6479a15a91ad5af6ef5d
Milena <milenliveira@gmail.com>
2020-10-12 23:07:46 -0300

    Gigabytes em bytes

--- Atividades/Atividade 01/03.c
+++ Atividades/Atividade 01/03.c
@@ -6,7 +6,21 @@
 
 int main () {
 
+    int valor;
+    unsigned long int x;
+    int bytes = 1073741824;
 
+    printf("\n");
+    printf("Digite um valor em Gygabites para ser convertido em Bytes: ");
+    scanf("%d", &valor);
+
+    x = valor * bytes;
+
+    printf("           ~~~~~~~~~\n");
+
+
+    printf("O resultado é: %u bytes\n", x);
+    printf("\n");
 
 
     return 0;
