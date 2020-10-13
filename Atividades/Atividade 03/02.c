f1abf415451a276993912d7d4f861d787bfcde47
Milena <milenliveira@gmail.com>
2020-10-13 19:06:40 -0300

    Fibonacci

--- Atividades/Atividade 03/02.c
+++ Atividades/Atividade 03/02.c
@@ -2,13 +2,26 @@
 números da série de Fibonacci (o próximo é a soma dos
 dois anteriores). */
 
-
 #include <stdio.h>
 
-int main () {
+int main()
+{
+
+    int anterior = 0;
+    int atual = 1;
+    int proximo;
 
 
+    printf("A listagem dos 10 primeiros números da série de Fibonacci são:\n");
 
+
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d\n", anterior);
+        proximo = anterior + atual;
+        anterior = atual;
+        atual = proximo;
+    }
 
     return 0;
 }
