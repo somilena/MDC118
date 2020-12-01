70b8940b5ee6bd36e3d379106a1b06d03bc1a453
Milena <milenliveira@gmail.com>
2020-12-01 17:30:22 -0300

    Aula sobre Entrada e Saída

---
+++ Aulas/06 - Entrada e Saida/ler_escrever_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Leitura de string
+// gets
+
+// Escrita de string
+// puts
+
+int main() 
+
+{
+    char nome[30]; // conjunto de 30 caractes array
+    gets(nome); // ler até achar \n
+    puts(nome); // escreve e acrescenta \n
+
+    return 0;
+}
