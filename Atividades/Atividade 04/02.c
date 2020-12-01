ae4de1d822fcd5c1b6b5600830f0102069049ba2
Milena <milenliveira@gmail.com>
2020-12-01 17:33:43 -0300

    Bissexto

--- Atividades/Atividade 04/02.c
+++ Atividades/Atividade 04/02.c
@@ -2,12 +2,10 @@
 verifique se ele é bissexto (é multiplo de 4 e 
 não é multiplo de 100, ou é múltiplo de 400) */
 
-
 #include <stdio.h>
 
-int main ()
+int main()
 {
-
 
     int ano;
 
@@ -15,7 +13,6 @@ int main ()
     scanf("%d", &ano);
 
     printf("            ~~~~~~~~~~~~~\n");
-
 
     if ((ano % 4 == 0) && (ano % 100 != 0) || (ano % 400 == 0))
     {
@@ -25,7 +22,6 @@ int main ()
     {
         printf("OPA! O ano não é bissexto :(!\n", ano);
     }
-    
 
     return 0;
 }
