63974424f037cb63c3f90b3a562e131682d751e1
Milena <milenliveira@gmail.com>
2020-12-01 17:25:18 -0300

    Qual é o menor numero?

---
+++ Atividades/Atividade 04/01.c
@@ -0,0 +1,40 @@
+/* Escrever um programa em C que leia dois 
+números inteiros e determine qual é o menor. */
+
+
+#include <stdio.h>
+
+int main ()
+{
+
+    int primeiro_numero;
+    int segundo_numero;
+
+
+    printf ("Digite o primeiro número inteiro: ");
+    scanf("%d", &primeiro_numero);
+    printf("                    ~~~~~\n");
+    printf ("Digite o segundo número inteiro: ");
+    scanf("%d", &segundo_numero);
+
+    printf("            ~~~~~~~~~~~~~~~~~~\n");
+
+    if (primeiro_numero > segundo_numero)
+    {
+        printf("%d > %d \n", primeiro_numero, segundo_numero);
+    }
+    else if (primeiro_numero < segundo_numero)
+    {
+        printf("%d > %d \n", segundo_numero, primeiro_numero);
+    }
+    else
+    {
+        printf("Os dois números são iguais!\n");
+    }
+    
+
+
+
+
+    return 0;
+}
