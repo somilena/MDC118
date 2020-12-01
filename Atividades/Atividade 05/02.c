629d641440ade98a694bf6873f3e2b1a2c60e842
Milena <milenliveira@gmail.com>
2020-12-01 17:35:45 -0300

    Matriz

--- Atividades/Atividade 05/02.c
+++ Atividades/Atividade 05/02.c
@@ -7,13 +7,10 @@ telefone. Considere a matriz igual a
 {5555, 30005000}, {6666, 30006000}, 
 {7777, 30007000}}. */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-int main ()
+int main()
 {
 
     int funcionario[7][2] = {{1111, 30001000}, {2222, 30002000}, {3333, 30003000}, {4444, 30004000}, {5555, 30005000}, {6666, 30006000}, {7777, 30007000}};
@@ -22,7 +19,7 @@ int main ()
     printf("Digite um número de 1 a 7 para identificar o número de matrícula do funcionário: ");
     scanf("%d", &matricula);
 
-    for(int i=0; i < 7; i++)
+    for (int i = 0; i < 7; i++)
     {
         printf("O número do funcionário é: %d\n", funcionario[i]);
     }
